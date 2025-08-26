aeaa93d3c8c217e41de62dc18de519fee16e8bf3
Michail Rontionov <mrontionov@mront.io>
2025-08-26 23:38:38 +0100

    Create new Tensor from HDF Group

--- src/main/scala/nir/Fensor.scala
+++ src/main/scala/nir/Fensor.scala
@@ -1,5 +1,8 @@
 package nir
 
+import io.jhdf.api.{Dataset}
+import scala.reflect.ClassTag
+
 
 case class Indexer(shape: List[Int]) {
   def rank: Int = shape.length
@@ -54,4 +57,40 @@ case class Indexer(shape: List[Int]) {
 class Fensor[D](data: Array[D], idx: Indexer) {
   def apply(indices: Int*) = data(idx(indices))
   def reshape(newshape: List[Int]): Fensor[D] = new Fensor[D](data, idx.reshape(newshape))
+}
+
+object Fensor {
+  def apply[D](a: Array[D], shape: List[Int]): Fensor[D] = {
+    val i = Indexer(shape)
+    if (i.size != a.length) throw new IllegalArgumentException(s"Supplied array is size ${a.length} but shape is over size ${i.size}.")
+    new Fensor[D](a, i)
+  }
+
+  private def flattenArray[T: ClassTag](a: Array[Object], dims: List[Int]): Array[T] = {
+    dims match {
+      case Nil                  => Array.empty[T]
+      case dim :: Nil         => a.asInstanceOf[Array[T]]
+      case dim1 :: dim2:: Nil => a.asInstanceOf[Array[Array[T]]].flatten.asInstanceOf[Array[T]]
+      case dim :: t           => a.asInstanceOf[Array[Array[Object]]].map(flattenArray(_, t)).flatten.asInstanceOf[Array[T]]
+    }
+  }
+
+  private def flattenDatasetArray[T: ClassTag](d: Dataset): Array[T] = {
+    val ndArray = d.getData.asInstanceOf[Array[Object]]
+    val dims = d.getDimensions.toList
+
+    flattenArray[T](ndArray, dims)
+  }
+
+  def apply(d: Dataset) = {
+    val idx = Indexer(d.getDimensions.toList)
+    println(idx)
+    d.getDataType.getJavaType.toString match {
+      case "float" => new Fensor(flattenDatasetArray[Float](d), idx)
+      case "int"   => new Fensor(flattenDatasetArray[Int](d), idx)
+      case "long"  => new Fensor(flattenDatasetArray[Long](d), idx)
+      case o => throw new java.text.ParseException(s"Java type \"${o}\" not yet supported for Tensor conversion", 0)
+    }
+
+  }
 }
--- src/main/scala/nir/Main.scala
+++ src/main/scala/nir/Main.scala
@@ -1,14 +1,29 @@
 package nir
 
 import java.io.File
+import io.jhdf.HdfFile
+import io.jhdf.api.{Attribute, Dataset, Group, Node}
+import scala.jdk.CollectionConverters._
 
 object Main extends App {
+  val default = "src/test/scala/nir/samples/conv1/network.nir"
+  var path = default
   if (args.length != 1) {
-    Console.err.println("Usage: sbt \"run <path/to/file.h5>\"")
-    sys.exit(1)
+    println(s"Using default $default")
+    path = default
   }
-  val file = new File(args(0))
+  val file = new File(path)
+  val hdf = new HdfFile(file)
+  val nodeHDF = hdf.getByPath("/node/nodes")
+  val d: Dataset = nodeHDF match {
+    case g1: Group => g1.getChild("0") match {
+      case g2: Group => g2.getChild("weight").asInstanceOf[Dataset]
+    }
+  }
 
-  val g: NIRGraph = NIRMapper.loadGraph(file)
-  println(g)
+  val f = Fensor(d)
+
+
+  println(f(15, 15, 2))
+
 }
